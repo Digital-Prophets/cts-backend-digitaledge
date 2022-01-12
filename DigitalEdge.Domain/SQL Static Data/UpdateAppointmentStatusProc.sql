30fd90d3cb6751e5e99960a246748e1486ce87ef
Gift JR <muyembegift@gmail.com>
2022-01-12 20:57:01 +0200

    Update on days late for missed appointments

--- DigitalEdge.Domain/SQL Static Data/UpdateAppointmentStatusProc.sql
+++ DigitalEdge.Domain/SQL Static Data/UpdateAppointmentStatusProc.sql
@@ -2,7 +2,7 @@ CREATE OR ALTER PROCEDURE [dbo].[UpdateAppointmentStatus]
 AS 
 BEGIN
 	DECLARE @Today AS datetime2 = getdate()
-	-- missed appointments
+	-- CASE 1: missed appointments
 	UPDATE app 
 		SET app.appointmentstatus = -1,
 			app.dateedited = @Today,
@@ -10,8 +10,13 @@ BEGIN
 	FROM [dbo].[Appointments] app 
 	WHERE app.appointmentdate < @Today 
 	AND app.interactiondate IS NULL AND app.appointmentstatus = 0;
+	-- update days late on missed appointments
+	UPDATE app
+		SET app.DaysLate = (SELECT datediff(day, app.appointmentdate, @Today))
+	FROM [dbo].[Appointments] app
+	WHERE app.AppointmentStatus = -1
 	
-	-- attended appointments
+	-- CASE 2: attended appointments
 	UPDATE app 
 		SET app.appointmentstatus = 1,
 			app.dateedited = @Today 
@@ -20,12 +25,13 @@ BEGIN
 	AND app.appointmentstatus = 0 
 	AND app.interactiondate > app.appointmentdate;
 	
-	-- pending appointments
+	-- CASE 3: pending appointments
 	UPDATE app 
 		SET app.appointmentstatus = 0, 
 		app.dateedited = @Today 
 	FROM [dbo].[Appointments] app 
 	WHERE app.interactiondate IS NULL AND app.appointmentdate > @Today;
+
 END 
 -- run procedure
 GO
